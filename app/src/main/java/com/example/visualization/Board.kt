ad9c9f55b383648e4066207e91b1ebf229aa2dec
KamilWyszynski1 <kamil.wyszynski.97@gmail.com>
2021-04-10 16:30:28 +0200

    trying adding dynamically tiles for grid inside layout

---
+++ app/src/main/java/com/example/visualization/Board.kt
@@ -0,0 +1,84 @@
+package com.example.visualization
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.drawable.ShapeDrawable
+import android.graphics.drawable.shapes.RectShape
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import kotlin.math.min
+
+class Board(context: Context?, attrs: AttributeSet?) : ViewGroup(context, attrs) {
+    override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
+
+    }
+
+    @SuppressLint("DrawAllocation")
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+        val VERTICAL_GAP = 100
+        val HORIZONTAL_GAP = 200
+        ///
+        val HORIZONTAL_SIZE = 30
+        val VERTIVAL_SIZE = 20
+
+        val left = VERTICAL_GAP
+        val top = 0
+        val gap = 10
+        val size = min(
+            (height - 2 * HORIZONTAL_GAP - HORIZONTAL_SIZE * gap) / HORIZONTAL_SIZE,
+            (width - 2 * VERTICAL_GAP - VERTIVAL_SIZE * gap) / VERTIVAL_SIZE
+        )
+
+        val paint = Paint()
+        paint.color = Color.RED
+
+        for (i in 0 until HORIZONTAL_SIZE) {
+            for (j in 0 until VERTIVAL_SIZE) {
+                // draw rectangle shape to canvas
+                val leftUP = left + (size + gap) * j // 100 210 320
+                val top2 = top + (size + gap) * i //
+                val right = leftUP + size
+                val bottom = top2 + size
+
+                canvas?.drawRect(
+                    leftUP.toFloat(),
+                    top2.toFloat(),
+                    right.toFloat(),
+                    bottom.toFloat(),
+                    paint
+                )
+            }
+        }
+
+//        addView(Tile(context, 0, 0))
+    }
+}
+
+class Tile(context: Context?, x: Int, y: Int) : View(context) {
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        setBackgroundColor(Color.GREEN)
+        return super.onTouchEvent(event)
+    }
+
+    @SuppressLint("DrawAllocation")
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        val paint: Paint = Paint()
+        paint.color = Color.GRAY
+        canvas?.drawRect(
+            0f,
+            0f,
+            0f,
+            0f,
+            paint
+        )
+    }
+}
--- app/src/main/java/com/example/visualization/FirstFragment.kt
+++ app/src/main/java/com/example/visualization/FirstFragment.kt
@@ -1,12 +1,23 @@
 package com.example.visualization
 
+import android.content.res.Resources
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.ShapeDrawable
+import android.graphics.drawable.shapes.OvalShape
+import android.graphics.drawable.shapes.RectShape
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.navigation.fragment.findNavController
+import kotlin.math.min
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -14,18 +25,82 @@ import androidx.navigation.fragment.findNavController
 class FirstFragment : Fragment() {
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        val root = inflater.inflate(R.layout.fragment_first, container, false)
+        val cl = root.findViewById<ConstraintLayout>(R.id.cl)
+        val bt = Button(context)
+        bt.text = "elo"
+        cl.addView(bt)
+        return root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        super.onCreate(savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        println(
+            "${Resources.getSystem().displayMetrics.heightPixels}, ${Resources.getSystem().displayMetrics.widthPixels}"
+        )
+
+        val height = Resources.getSystem().displayMetrics.heightPixels
+        val width = Resources.getSystem().displayMetrics.widthPixels
+
+        val VERTICAL_GAP = 100
+        val HORIZONTAL_GAP = 200
+        ///
+        val HORIZONTAL_SIZE = 10
+        val VERTIVAL_SIZE = 10
+
+        val bitmap: Bitmap = Bitmap.createBitmap(700, 1000, Bitmap.Config.ARGB_8888)
+        val canvas: Canvas = Canvas(bitmap)
+
+        var shapeDrawable: ShapeDrawable
+
+        val a = (height - 2 * HORIZONTAL_GAP) / HORIZONTAL_GAP
+        // rectangle positions
+        val left = VERTICAL_GAP
+        val top = 0
+        val gap = 10
+        val size = min(
+            (height - 2 * HORIZONTAL_GAP - HORIZONTAL_SIZE * gap) / HORIZONTAL_SIZE,
+            (width - 2 * VERTICAL_GAP - VERTIVAL_SIZE * gap) / VERTIVAL_SIZE
+        )
+
+//        shapeDrawable = ShapeDrawable(RectShape())
+//
+//        shapeDrawable.setBounds(
+//            50,
+//            100,
+//            110,
+//            110
+//        )
+//        shapeDrawable.getPaint().setColor(Color.parseColor("#009944"))
+//        shapeDrawable.draw(canvas)
+
+        for (i in 0 until HORIZONTAL_SIZE) {
+            for (j in 0 until VERTIVAL_SIZE) {
+                // draw rectangle shape to canvas
+                shapeDrawable = ShapeDrawable(RectShape())
+                val leftUP = left + (size + gap) * j // 100 210 320
+                val top2 = top + (size + gap) * i //
+                val right = leftUP + size
+                val bottom = top2 + size
+                shapeDrawable.setBounds(
+                    leftUP,
+                    top2,
+                    right,
+                    bottom
+                )
+                shapeDrawable.getPaint().setColor(Color.parseColor("#009944"))
+                shapeDrawable.draw(canvas)
+            }
         }
+
+        val iView: ImageView = view.findViewById<ImageView>(R.id.imageV)
+        println("${iView.resources.displayMetrics.heightPixels}, ${iView.resources.displayMetrics.widthPixels}")
+        iView.setImageBitmap(bitmap)
     }
 }
